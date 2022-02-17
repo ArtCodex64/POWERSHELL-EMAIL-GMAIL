f2e4d8691cb6165c7159b2791f00af2f9a61d425
ArtCodex64 <89210065+ArtCodex64@users.noreply.github.com>
2022-02-17 18:19:46 +0100

    Version 1.0.0.0
    Script ejemplo

---
+++ enviar-correo-gmail-smtp.ps1
@@ -0,0 +1,19 @@
+﻿$mq = hostname
+
+$usr = whoami
+
+$BODY = "Maquina: " + $mq + " Usuario: " + $usr
+
+$FROM = "correo-envia@dominio.com"
+
+$TO = "correo-destinatario@dominio.com"
+
+$Subject = "Mensaje de prueba"
+
+$SMTPServer = "smtp.gmail.com"
+
+$SMTPPort = 587
+
+$credential = Get-Credential
+
+Send-MailMessage -SmtpServer $SMTPServer -Port $SMTPPort -UseSsl -From $FROM -To $TO -Subject $Subject -Body $BODY -Credential $credential
